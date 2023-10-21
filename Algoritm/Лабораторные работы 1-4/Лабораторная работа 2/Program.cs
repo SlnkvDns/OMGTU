ddbc3e49ae7a324b61047d0e7e2e95b60a282e32
SlnkvDns <144824977+SlnkvDns@users.noreply.github.com>
2023-10-21 19:50:09 +0600

    Add files via upload

---
+++ Algoritm/Лабораторные работы 1-4/Лабораторная работа 2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        double x = Convert.ToDouble (Console.ReadLine());
+        double a = 2.3, f = 0;
+        if (x >= 0 && x <= 4)
+        {
+            if (x > 2)
+                f = Math.Pow(x + a, 1.0 / 5);
+            else if (x > 0.3 && x <= 2)
+                f = x;
+            else if (x <= 0.3)
+                f = Math.Cos(x - a);
+            Console.WriteLine(f);
+        }
+        else
+            Console.WriteLine("x не принадлежит [0, 4]");
+    }
+}
