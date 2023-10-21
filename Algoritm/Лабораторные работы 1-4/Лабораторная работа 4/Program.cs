e70dbdddc9d1028221de498cc89a4ed83dacdc35
SlnkvDns <144824977+SlnkvDns@users.noreply.github.com>
2023-10-21 19:53:30 +0600

    Add files via upload

---
+++ Algoritm/Лабораторные работы 1-4/Лабораторная работа 4/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        double[] mas = { 1, 1.5 };
+        double y = 0;
+        foreach (double a in mas)
+        {
+            Console.WriteLine($"Для a = {a}:");
+            for (double x = 0.1; x <= 1.6; x += a / 5.0)
+            {
+                if (x >= 0.1 && x <= 0.4)
+                    y = Math.Sin(a * x) - Math.Sqrt(Math.Log10(a * a + x * x));
+                else if (x > 0.4 && x <= 1.2)
+                    y = a * Math.Cos(Math.PI * x) * Math.Cos(Math.PI * x);
+                else if (x > 1.2 && x <= 1.6)
+                    y = 2 - Math.Sin(a * x);
+                Console.WriteLine($"x = {Math.Round(x,2)}: {y}"); 
+            }
+            Console.WriteLine();
+        }
+    }
+}
