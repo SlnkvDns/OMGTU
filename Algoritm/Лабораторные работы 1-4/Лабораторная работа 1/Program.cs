1359ba8536a3fe7edaab6ca8596aea25a44f7f8d
SlnkvDns <144824977+SlnkvDns@users.noreply.github.com>
2023-10-21 19:47:50 +0600

    Add files via upload

---
+++ Algoritm/Лабораторные работы 1-4/Лабораторная работа 1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        double x = Convert.ToDouble(Console.ReadLine());
+        double a = 0.7, c = 2.1;
+        double y = Math.Sin(x) / Math.Sqrt(1 + a * a * Math.Sin(x) * Math.Sin(x)) - c * Math.Log(a * x);
+        Console.WriteLine(y);
+    }
+}
+
