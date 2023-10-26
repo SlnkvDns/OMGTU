0a70d5333578f24a2a6e455937bf4bbf17df0799
SlnkvDns <144824977+SlnkvDns@users.noreply.github.com>
2023-10-26 22:40:57 +0600

    Add files via upload

---
+++ Olimpiad/24.10.2023_otborVrazvetku.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int n = Convert.ToInt32 (Console.ReadLine());
+        int[] mas = new int[n+1];
+        mas[3] = 1;
+        for (int i = 4; i < mas.Length; i++)
+            mas[i] = mas[i / 2] + mas[i - i / 2];
+        Console.WriteLine(mas[n]);
+    }
+}
