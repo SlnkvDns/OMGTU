05a367c7c8a29a3c04150dc97a0a0eb976d57a07
SlnkvDns <144824977+SlnkvDns@users.noreply.github.com>
2024-02-25 19:54:01 +0600

    Update CompanyX.py

--- Olimpiad/CompanyX.py
+++ Olimpiad/CompanyX.py
@@ -1,39 +1,13 @@
 from sys import setrecursionlimit
-from functools import lru_cache
-setrecursionlimit(20000)
+import threading
 
-for k in range(1, 17):
-    sotrudniki = {}
-    sotrudniki2 = {}
-    with open(f"D:\\comp\\input_s1_{k:02}.txt") as f, open(f"D:\\comp\\output_s1_{k:02}.txt") as f2:
-        s1 = f.readline().split()
-        if s1[0] not in sotrudniki2.keys():
-            if len(s1) == 1:
-                sotrudniki2[s1[0]] = "Unknown Name"
-            else:
-                sotrudniki2[s1[0]] = " ".join(s1[1:])
-        else:
-            if sotrudniki2[s1[0]] == "Unknown Name" and len(s1) != 1:
-                sotrudniki2[s1[0]] = " ".join(s1[1:])
-        if len(s1) != 1:
-            s1 = s1[0:1]
-        s2 = f.readline().split()
-        if s2[0] not in sotrudniki2.keys():
-            if len(s2) == 1:
-                sotrudniki2[s2[0]] = "Unknown Name"
-            else:
-                sotrudniki2[s2[0]] = " ".join(s2[1:])
-        else:
-            if sotrudniki2[s2[0]] == "Unknown Name" and len(s2) != 1:
-                sotrudniki2[s2[0]] = " ".join(s2[1:])
-        if len(s2) != 1:
-            s2 = s2[0:1]
-        sotrudniki[" ".join(s1)] = [" ".join(s2)]
-
-        while True:
+def main():
+    for k in range(1, 17):
+        global lst
+        sotrudniki = {}
+        sotrudniki2 = {}
+        with open(f"C:\\Users\\Denis\\Downloads\\Kompania_KhKhKh\\Компания ХХХ\\input_s1_{k:02}.txt") as f, open(f"C:\\Users\\Denis\\Downloads\\Kompania_KhKhKh\\Компания ХХХ\\output_s1_{k:02}.txt") as f2:
             s1 = f.readline().split()
-            if s1[0] == "END":
-                break
             if s1[0] not in sotrudniki2.keys():
                 if len(s1) == 1:
                     sotrudniki2[s1[0]] = "Unknown Name"
@@ -42,8 +16,7 @@ for k in range(1, 17):
             else:
                 if sotrudniki2[s1[0]] == "Unknown Name" and len(s1) != 1:
                     sotrudniki2[s1[0]] = " ".join(s1[1:])
-            if len(s1) != 1:
-                s1 = s1[0:1]
+            s11 = s1[0]
 
             s2 = f.readline().split()
             if s2[0] not in sotrudniki2.keys():
@@ -54,51 +27,75 @@ for k in range(1, 17):
             else:
                 if sotrudniki2[s2[0]] == "Unknown Name" and len(s2) != 1:
                     sotrudniki2[s2[0]] = " ".join(s2[1:])
+            s22 = s2[0]
 
-            if len(s2) != 1:
-                s2 = s2[0:1]
+            sotrudniki[s11] = [s22]
 
-            if " ".join(s1) not in sotrudniki.keys():
-                sotrudniki[" ".join(s1)] = [" ".join(s2)]
-            else:
-                sotrudniki[" ".join(s1)].append(" ".join(s2))
+            while True:
+                s1 = f.readline().split()
+                s11 = s1[0]
+                if s11 == "END":
+                    break
+                if s1[0] not in sotrudniki2.keys():
+                    if len(s1) == 1:
+                        sotrudniki2[s1[0]] = "Unknown Name"
+                    else:
+                        sotrudniki2[s1[0]] = " ".join(s1[1:])
+                else:
+                    if sotrudniki2[s1[0]] == "Unknown Name" and len(s1) != 1:
+                        sotrudniki2[s1[0]] = " ".join(s1[1:])
 
-        lst = set()
-        @lru_cache(None)
-        def find(name):
-            global lst
-            if name not in sotrudniki.keys():
-                lst.add(name)
-                return
-            for i in sotrudniki[name]:
-                lst.add(i)
-                find(i)
+                s2 = f.readline().split()
+                if s2[0] not in sotrudniki2.keys():
+                    if len(s2) == 1:
+                        sotrudniki2[s2[0]] = "Unknown Name"
+                    else:
+                        sotrudniki2[s2[0]] = " ".join(s2[1:])
+                else:
+                    if sotrudniki2[s2[0]] == "Unknown Name" and len(s2) != 1:
+                        sotrudniki2[s2[0]] = " ".join(s2[1:])
+                s22 = s2[0]
+                if s11 not in sotrudniki.keys():
+                    sotrudniki[s11] = [s22]
+                else:
+                    sotrudniki[s11].append(s22)
+            name = f.readline()
+            if any(x not in "0123456789" for x in name):
+                for i, j in sotrudniki2.items():
+                    if j == name:
+                        name = i
+
+            lst = set()
+            def find(name):
+                if name not in sotrudniki.keys():
+                    lst.add(name)
+                    return
+                for i in sotrudniki[name]:
+                    lst.add(i)
+                    find(i)
 
 
-        name = f.readline()
-        if any(x not in "0123456789" for x in name):
-            for i, j in sotrudniki2.items():
-                if j == name:
-                    name = i
-        find(name)
+            find(name)
 
-        lst1 = list(lst)
-        lst1.sort()
-        if name in lst1:
-            lst1.remove(name)
-        lst1 = list(map(lambda x: x + " " + sotrudniki2[x], lst1))
-        if len(lst1) == 0:
-            lst1.append('NO')
-        flag = True
-        test = f2.readlines()
-        for l in range(len(test)):
-            test[l] = test[l].replace("\n", "")
-        for i in range(len(test)):
-            if lst1[i] != test[i]:
-                flag = False
-                break
-        print(f"Test{k}: {flag}")
+            lst1 = sorted(list(lst))
+            if name in lst1:
+                lst1.remove(name)
+            lst1 = list(map(lambda x: x + " " + sotrudniki2[x], lst1))
+            if len(lst1) == 0:
+                lst1.append('NO')
+
+            flag = True
+            test = f2.readlines()
+            for l in range(len(test)):
+                test[l] = test[l].replace("\n", "")
+            for i in range(len(test)):
+                if lst1[i] != test[i]:
+                    flag = False
+                    break
+            print(f"Test{k}: {flag}")
 
 
-
-
+setrecursionlimit(20000)
+threading.stack_size(2 ** 26)
+thread = threading.Thread(target=main)
+thread.start()
